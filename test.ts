0a520d4b77303584c02fff72205fc3d32134b109
ankushinfomaticae2023 <ankush@infomaticae.com>
2025-01-12 06:03:50 +0530

    test

--- test.ts
+++ test.ts
@@ -1,10 +1,11 @@
-// Function to calculate the difference of two numbers
-const sum = (c: number, d: number): number => {
-  // Subtract second number from the first
+// Function to perform subtraction operation
+const sub = (c: number, d: number): number => {
+  // Calculate the result
   const result = c - d;
+  
   // Return the result
   return result;
 }
 
-// Log the result of the function with inputs 2 and 4
-console.log(sum(2,4))
+// Test the function
+console.log(sub(2,4))
