b586c989c4e104b6c2820287275d2fa0776ebd58
ankushinfomaticae2023 <ankush@infomaticae.com>
2025-01-12 12:20:27 +0530

    test

--- test.ts
+++ test.ts
@@ -1,12 +1,11 @@
-```typescript
 // Function to subtract two numbers
 const sub = (c: number, d: number): number => {
   // Subtracting d from c
   const result = c - d;
-  // Returning the result
+  
+  // Return the result
   return result;
 }
 
-// Logging the result of subtraction of 4 from 2
+// Log the result of the subtraction
 console.log(sub(2,4))
-```
