af673774c2886d70147f6c5d6141e43bb90f7b2d
ankushinfomaticae2023 <ankush@infomaticae.com>
2025-01-12 06:03:08 +0530

    test

--- test.ts
+++ test.ts
@@ -1,6 +1,10 @@
+// Function to calculate the difference of two numbers
 const sum = (c: number, d: number): number => {
+  // Subtract second number from the first
   const result = c - d;
+  // Return the result
   return result;
 }
 
+// Log the result of the function with inputs 2 and 4
 console.log(sum(2,4))
