8fc2c3b774c560acd30979035a4d75b06d9ab2f0
ankushinfomaticae2023 <ankush@infomaticae.com>
2025-01-12 05:41:46 +0530

    test

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
-const sub = (c: number, d: number): number => {
+const sum = (c: number, d: number): number => {
   const result = c - d;
   return result;
 }
 
-console.log(sub(2,4))
+console.log(sum(2,4))
