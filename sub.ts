f6bc73c81b3e551ab0da90f9823f636abdec1be5
ankushinfomaticae2023 <ankush@infomaticae.com>
2025-01-12 12:22:55 +0530

    test

---
+++ sub.ts
@@ -0,0 +1,9 @@
+// Function to add two numbers
+const subs = (c: number, d: number): number => {
+    // Calculate the sum of input numbers
+    const result = c + d;
+    // Return the sum
+    return result;
+}
+// Call the function and log the result
+console.log(subs(2,4));
---
+++ sum.ts
@@ -0,0 +1,11 @@
+// Function to calculate sum of two numbers
+const sum = (c: number, d: number): number => {
+    // Calculate and store result of sum
+    const result = c + d;
+
+    // Return the result
+    return result;
+}
+
+// Call and log result of sum function
+console.log(sum(2,4))
