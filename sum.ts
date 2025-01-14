fa923c32052c9e4b4892b116e49ec3f2043a684e
ankushinfomaticae2023 <ankush@infomaticae.com>
2025-01-14 23:52:10 +0530

    test

--- sum.ts
+++ sum.ts
@@ -1,4 +1,4 @@
-// Function to calculate the sum of two numbers
+// Define a function to calculate the sum of two numbers
 const sum = (c: number, d: number): number => {
     // Calculate the sum of the two input numbers
     const result = c + d;
@@ -6,5 +6,5 @@ const sum = (c: number, d: number): number => {
     return result;
 }
 
-// Call the sum function and log the result
+// Call the sum function with the numbers 2 and 4 and log the result to the console
 console.log(sum(2,4))
